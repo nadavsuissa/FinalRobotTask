35d466d20518567c426c355d1c7fed2d6a5250fb
NadavSuissa <74867230+nadavsuissa@users.noreply.github.com>
2022-07-13 13:09:01 +0300

    more

--- BlasterIdentify.py
+++ BlasterIdentify.py
@@ -1,5 +1,7 @@
 # Make Robomaster fire his blaster every time he sees a person. Set the vision marker
 # detection distance from 0.5 to 3 for farther distances.
+from random import random
+
 
 def start():
     # Blaster fire example function:
@@ -26,19 +28,16 @@ def start():
             vision_ctrl.cond_wait(rm_define.cond_recognized_people)
 
             while True:
-                randgimbal_speed = random.randint(20, 100) // 30
-                randup = random.randint(1, 55) // 20
 
-                gimbal_ctrl.set_rotate_speed(randgimbal_speed)
-
+                gimbal_ctrl.set_rotate_speed(55)
+                # Rotate sound
                 media_ctrl.play_sound(rm_define.media_sound_gimbal_rotate, wait_for_complete_flag=False)
-
+                # Flash
                 led_ctrl.set_flash(rm_define.armor_all, 4)
-
                 led_ctrl.set_top_led(rm_define.armor_top_all, 255, 0, 0, rm_define.effect_marquee)
                 led_ctrl.set_bottom_led(rm_define.armor_bottom_all, 255, 0, 0, rm_define.effect_flash)
 
-                gimbal_ctrl.rotate_with_degree(rm_define.gimbal_up, randup)
+                gimbal_ctrl.rotate_with_degree(rm_define.gimbal_up, 35)
 
                 led_ctrl.set_flash(rm_define.armor_all, 9)
                 led_ctrl.set_top_led(rm_define.armor_top_all, 0, 255, 255, rm_define.effect_flash)
